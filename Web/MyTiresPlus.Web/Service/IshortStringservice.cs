69f7d165a9a1688d5e01c6d94d68700215c01685
Svetoslav Gochev <svetoslav.gochev@gmail.com>
2021-07-03 23:30:23 +0300

    dsxf

---
+++ Web/MyTiresPlus.Web/Service/IshortStringservice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTiresPlus.Web.Service
+{
+    public interface IshortStringservice
+    {
+        string GetShort(string str, int maxLength);
+    }
+
+    public class ShortStringservice : IshortStringservice
+    {
+        public string GetShort(string str, int maxLength)
+        {
+            if (str == null)
+            {
+                return str;
+            }
+
+            if (str.Length <= maxLength)
+            {
+                return str;
+            }
+
+            return str.Substring(0, maxLength) + "...";
+            //trqbva da se registrira v start up public void ConfigureServices(IServiceCollection services)
+        }
+    }
+}
