43173a63f263852366a973944272c6a63058fb98
Svetoslav Gochev <svetoslav.gochev@gmail.com>
2021-07-05 08:37:43 +0300

    DFTY

--- Web/MyTiresPlus.Web/ViewModels/Home/IndexViewModel.cs
+++ Web/MyTiresPlus.Web/ViewModels/Home/IndexViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace MyTiresPlus.Web.ViewModels.Home
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTiresPlus.Web.ViewModels.Home
 {
     public class IndexViewModel
     {
         //opisvame danite koito mu trqbvat na vito
+        
+        
+        [Range(2000, 2100)]
         public int Year { get; set; }
 
         public int UserCount { get; set; }
@@ -12,7 +17,7 @@
         public int DataTime { get; set; }
 
         public int Id { get; set; }
-
+        [DataType(DataType.MultilineText)]//za mnoigoredvi poleta
         public string Email { get; set; }
 
         public string Name { get; set; }
